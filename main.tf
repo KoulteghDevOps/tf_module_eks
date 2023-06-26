07e5ee6ffc0a80ff8fb7719c4e8adabba5ced608
KoulteghdevOps <koultegh@outlook.com>
2023-06-26 13:19:05 +0100

    manifest pushed

--- main.tf
+++ main.tf
@@ -44,4 +44,7 @@ resource "aws_iam_openid_connect_provider" "cluster" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.external.thumb.result.thumbprint]
   url             = aws_eks_cluster.eks.identity.0.oidc.0.issuer
+
 }
+
+
